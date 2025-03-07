333d29f008c40dbaaebde87f6fc64a6224f138c0
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2025-03-06 22:37:21 -0500

    feat(shadcn): :wrench: Add shadcn helper functions

---
+++ app/hooks/useMediaQuery.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+export function useMediaQuery(query: string) {
+  const [value, setValue] = React.useState(false);
+
+  React.useEffect(() => {
+    function onChange(event: MediaQueryListEvent) {
+      setValue(event.matches);
+    }
+
+    const result = matchMedia(query);
+    result.addEventListener("change", onChange);
+    setValue(result.matches);
+
+    return () => result.removeEventListener("change", onChange);
+  }, [query]);
+
+  return value;
+}
+
+export function useIsMobile() {
+  return useMediaQuery("(max-width: 768px)");
+}
+
+export function useIsTablet() {
+  return useMediaQuery("(max-width: 1024px)");
+}
+
+export function useIsDesktop() {
+  return useMediaQuery("(min-width: 1024px)");
+}
