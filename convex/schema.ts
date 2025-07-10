08ef66a348747283a5a67c9eb3015cd473c7ed2e
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2025-07-09 19:04:09 -0500

    feat(convex): :sparkles: Add additional indexes for banks and vehicles - Added indexes for bankId, color, price, and composite indexes for vehicleId and bankId. - Introduced indexes for make, model, and year in the vehicles table for improved query performance.

--- convex/schema.ts
+++ convex/schema.ts
@@ -26,11 +26,21 @@ export default defineSchema({
     id: v.string(),
     price: v.union(v.null(), v.float64()),
     vehicleId: v.string(),
-  }),
+  })
+    .index("by_vehicleId", ["vehicleId"])
+    .index("by_bankId", ["bankId"])
+    .index("by_color", ["color"])
+    .index("by_price", ["price"])
+    .index("by_vehicleId_bankId", ["vehicleId", "bankId"]),
   vehicles: defineTable({
     id: v.string(),
     make: v.string(),
     model: v.string(),
     year: v.float64(),
-  }),
+  })
+    .index("by_make", ["make"])
+    .index("by_model", ["model"])
+    .index("by_year", ["year"])
+    .index("by_make_model", ["make", "model"])
+    .index("by_make_year", ["make", "year"]),
 });
